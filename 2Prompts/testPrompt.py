808ed0d270378b90fa9df77c45b1b5c3c86a0a6e
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-07-15 12:57:39 +0530

    lgtm

---
+++ 2Prompts/testPrompt.py
@@ -0,0 +1,24 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+load_dotenv()
+envValue = os.getenv("HUGGING_FACE_API_KEY")
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    huggingfacehub_api_token= envValue
+)
+
+prompt_template = PromptTemplate.from_template("hey there how's the weather in {place}")
+userInput = str(input("Enter your location :"))
+
+prompt = prompt_template.format(place = userInput)
+response = llm.invoke(prompt)
+print(response)
+
