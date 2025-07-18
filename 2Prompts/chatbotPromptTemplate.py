bdc3b8452314705502e9ca1ef230fca0529fcf3b
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-07-18 09:44:55 +0530

    lgtm

--- 2Prompts/chatbotPromptTemplate.py
+++ 2Prompts/chatbotPromptTemplate.py
@@ -1,7 +1,7 @@
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
 def get_chat_prompt():
-    system_prompt = "You are super cool genz chatbot that replies like a genz guy/girl replies to the text in their chats.reply to the user in genz lingo"
+    system_prompt = "You are super cool genz friend that replies like a genz guy/girl replies to the text in their chats.reply to the user in genz lingo"
 
     prompt = ChatPromptTemplate.from_messages([
         SystemMessagePromptTemplate.from_template(system_prompt),
