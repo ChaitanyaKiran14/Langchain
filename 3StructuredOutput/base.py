25502f3476104e3e788da9f440d7b917516b791f
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-07-20 21:17:56 +0530

    added structured output

---
+++ 3StructuredOutput/base.py
@@ -0,0 +1,13 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+import os
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GEMINI_API_KEY')
+
+
+
+##model = init_chat_model(model="gemini-2.5-pro",  google_api_key=os.getenv("GEMINI_API_KEY"))
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+output = model.invoke("Hello, world!")
+print(output.content)
