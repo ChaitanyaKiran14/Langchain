ea7536aeabd217a3ab7b70a2a35130ae130e9fd4
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-07-21 09:38:31 +0530

    lgtm

--- 3StructuredOutput/pydantrixxx.py
+++ 3StructuredOutput/pydantrixxx.py
@@ -4,10 +4,15 @@ from typing_extensions import Optional
 
 class Student(BaseModel):
     name : str = "kiran"
-    age : Optional[int] = None
+    age : Optional[int] = 22
     email : EmailStr
     cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
 
 new_student =  {"name": "Hey Chaitu", "email" : "chaitu@gmail.com", "cgpa" : 9}
 student = Student(**new_student)
 print(student)
+
+student_dict = dict(student)
+print(student_dict['age'])
+student_json = student.model_dump_json()
+print(student_json)
