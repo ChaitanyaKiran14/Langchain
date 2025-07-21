cbbe4fde39f48382bfab54c98d6fdecd45614364
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-07-21 09:35:26 +0530

    pydantic

--- 3StructuredOutput/pydantrixxx.py
+++ 3StructuredOutput/pydantrixxx.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr,Field
 from typing_extensions import Optional
 
 
 class Student(BaseModel):
     name : str = "kiran"
-    age : Optional[int]
+    age : Optional[int] = None
+    email : EmailStr
+    cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
 
-new_student =  {}
+new_student =  {"name": "Hey Chaitu", "email" : "chaitu@gmail.com", "cgpa" : 9}
 student = Student(**new_student)
 print(student)
