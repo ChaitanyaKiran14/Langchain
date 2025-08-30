9344e641fb7543ba7d9136252170712d10e01708
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2025-08-30 12:04:01 +0530

    lgtm

---
+++ 6Runnables/runnableBranch.py
@@ -0,0 +1,40 @@
+## so this is bascically for making conditional chains
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+from dotenv import load_dotenv
+import os
+from langchain.chat_models import init_chat_model
+
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
+
+
